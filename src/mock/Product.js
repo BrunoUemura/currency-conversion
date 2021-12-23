d010b7ba8a6660e3af5bfebd340ed5747cf0b52a
BrunoUemura <bruno.h.uemura@outlook.com>
2021-12-23 02:27:35 -0300

    Add responsiveness

--- src/mock/Product.js
+++ src/mock/Product.js
@@ -23,5 +23,30 @@ export const products = [
         },
         review: 4.5,
         reviewCount: 340
+    },
+    {
+        name: "Tênis Nike Air Max 90 - Masculino",
+        price: 649.99,
+        discount: 0.10,
+        image:
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fartwalk.vteximg.com.br%2Farquivos%2Fids%2F179256-1000-1000%2FTenis-Nike-Air-Max-90-OG-Masculino.jpg&f=1&nofb=1",
+        attributes: {
+            colors: ["black", "white", "red", "blue"],
+        },
+        review: 4.5,
+        reviewCount: 1214
+    },
+    {
+        name: "Tenis Mizuno Pro 6 - Masculino",
+        price: 999.99,
+        discount: 0.20,
+        currency: "BRL",
+        image:
+            "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fhttp2.mlstatic.com%2Ftenis-corrida-mizuno-pro-6-nova-roxorosa-frete-gratis-D_NQ_NP_658043-MLB27020022176_032018-F.jpg&f=1&nofb=1",
+        attributes: {
+            colors: ["red", "blue", "purple"],
+        },
+        review: 4,
+        reviewCount: 448
     }
 ];
